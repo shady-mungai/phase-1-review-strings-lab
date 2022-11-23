e2b323ef13dc7e2787c38a0086ba641d0144c5a7
shady-mungai <shadrackmungai10@gmail.com>
2022-11-23 13:01:12 +0300

    Completed Assingment On Strings lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+const currentUser = "Shady Mungai";
+
+//const welcomeMessage = 'Welcome to Flatbook, ';
+
+//string concatenation
+//const welcomeMessage = 'Welcome to Flatbook, '+ currentUser;
+
+// string interpolation
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//use of uppercace
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//short greting
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+// console.log(currentUser.slice(0, 1));
